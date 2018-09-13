eb68432d757873580c3142021eb77a7d1b38cde1
SSENENGO JACKSON <senjackglobal@gmail.com>
2018-09-13 10:22:59 +0300

    Added Content to the repository

---
+++ client_scripts/agropex_js.js
@@ -0,0 +1,15 @@
+/*WELCOME TO AGROPEX JAVASCRIPT MAIN CODE*/
+
+
+/*[1.] : DESIGNING A HOVER EFFECT*/
+function hvr(a){
+  var b = window.document.getElementById(a)
+  b.style.color = "orange";
+  b.style.background = "#fff";
+  b.style.borderRadius = "5px";
+}
+function out(a){
+  var b = window.document.getElementById(a)
+  b.style.color = "#fff";
+  b.style.background = "";
+}
